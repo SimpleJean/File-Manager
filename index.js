e6f19f560703f884c8117a7778e486923cda703b
Jean <zhan.yessim@gmail.com>
2022-06-12 20:35:15 +0600

    feat: up, ls task is done

---
+++ index.js
@@ -0,0 +1,54 @@
+import fs from 'node:fs';
+import fsp from 'node:fs/promises';
+import rl from 'readline';
+import process from 'node:process';
+import os from 'node:os';
+import path from 'node:path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const readLine = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log(process.cwd());
+// Get a name of user -- --username=your_username
+const getNameFromArgs = () => {
+  const args = process.argv.slice(2);
+  const name = args.find((arg) => arg.includes('username'));
+  if (name) {
+    console.log(`Welcome to the File Manager, ${name.split('=')[1]}`);
+    return name.split('=')[1];
+  } else {
+    console.log(`Welcome to the File Manager, ${os.userInfo().username}`);
+  }
+  return 'Username';
+};
+getNameFromArgs();
+
+//Change the current directory WORK
+readLine.on('line', (data) => {
+  if (data === 'up') {
+    try {
+      process.chdir('..');
+      console.log(`New directory: ${process.cwd()}`);
+    } catch (err) {
+      console.error(`chdir: ${err}`);
+    }
+    //Print the current files in that directory
+  } else if (data === 'ls') {
+    try {
+      fsp.readdir('./').then((files) => {
+        !files.length ? console.log('Folder is empty') : console.log(files);
+      });
+    } catch (err) {
+      console.error(`readdir: ${err}`);
+    }
+  }
+});
